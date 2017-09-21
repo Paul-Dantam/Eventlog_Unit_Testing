a13b84e07807e3de3d524398d888053166f905ac
Paul Dantam <pauldantam@Pauls-MacBook-Pro.local>
2017-09-21 13:27:24 -0500

    init and junit import

---
+++ src/com/TheIronYard/Event.java
@@ -0,0 +1,9 @@
+package com.TheIronYard;
+
+public class Event {
+
+    String name;
+    String action;
+
+
+}
---
+++ src/com/TheIronYard/EventLog.java
@@ -0,0 +1,37 @@
+package com.TheIronYard;
+
+import java.util.List;
+
+public class EventLog {
+
+
+
+
+
+
+
+    private List<Event> eventList;
+
+    public boolean addEvent(Event event){
+
+//        Throw an IllegalArgumentException if the event parameter is null or if either if the two variables of event are null.
+//
+//                If both parameters are set then print the event to the console and add it to the list.
+//
+//                The return value from the addEvent method should be true.
+
+
+    }
+
+    public int getNumEvents (){
+
+
+    }
+
+
+    public EventLog (){
+
+    }
+
+
+}
